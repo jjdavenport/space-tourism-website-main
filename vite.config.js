cb3e0724afe0f5c8de118858659807e764687991
jjdavenport <jordanjamesdavenport@gmail.com>
2024-08-03 23:30:51 +0100

    vite config correction

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/space-tourism-website-main",
+  base: "/space-tourism-website-main/",
 });
